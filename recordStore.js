c147458f1bb4bcb060697b4df7489ee4f007db96
AMacFadyen <ferretsbite@hotmail.co.uk>
2017-12-03 23:55:48 +0000

    financial statment function working with test

--- recordStore.js
+++ recordStore.js
@@ -18,13 +18,21 @@ RecordStore.prototype.listInventory = function () {
     return this.inventory;
 };
 
+RecordStore.prototype.stockSum = function () {
+    var stockValue = 0
+    for(record of this.inventory){
+        stockValue += record.price;
+    };
+    return stockValue;
+};
+
 RecordStore.prototype.sellRecord = function (record, index) {
     this.inventory.splice(index, 1);
     this.balance+= record.price;
 };
 
-RecordStore.prototype.methodName = function () {
-
+RecordStore.prototype.financialStatment = function () {
+    return "Current Balance: £" + (this.balance/100) + ", Current Inventory Value: £" + (this.stockSum()/100);
 };
 
 module.exports = RecordStore;
